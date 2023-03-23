ca357987f3e86803cbcec096804532c4da2b9298
Daniel <gendik38@gmail.com>
2023-03-23 17:40:57 +0100

    blog card content now responsive issue fixed

--- src/components/Blog/Blog.tsx
+++ src/components/Blog/Blog.tsx
@@ -6,7 +6,7 @@ import Card4 from "../Blogcards/Card4"
 
 const Blog: React.FC = () => {
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-2 gap-5 mt-10 ">
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-5 mt-10 ">
       <div className="grid-item-1 shadow-md ">
         <Card1 />
       </div>
--- src/components/Blogcards/Card2.tsx
+++ src/components/Blogcards/Card2.tsx
@@ -3,7 +3,7 @@ import picture2 from "../../assets/images/picture2.jpg"
 
 const Card2 = () => {
   return (
-    <div className="grid grid-cols-1 grid-rows-3">
+    <div className="grid grid-cols-1 grid-rows-3 md:grid-rows-2">
       <div className="image-ctn row-span-2">
         <img src={picture2} alt="" />
       </div>
