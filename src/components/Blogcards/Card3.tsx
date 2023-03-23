90bf1bc7186a0b4bd745830d0f5e3791ec360f85
Daniel <gendik38@gmail.com>
2023-03-23 17:12:55 +0100

    add description content and button to each card

--- src/components/Blogcards/Card3.tsx
+++ src/components/Blogcards/Card3.tsx
@@ -1,16 +1,18 @@
 import React from "react";
-import picture3 from "../../assets/images/picture3.jpg"
+import picture4 from "../../assets/images/picture4.jpg"
 
 
 const Card3 = () => {
   return (
     <div className="grid grid-cols-1 grid-rows-3">
     <div className="image-ctn row-span-2">
-        <img src={picture3} alt="" />
+        <img src={picture4} alt="" />
     </div>
-    <div className="card-content-descirption row-span-1 m-5">
-        <h1>blablbalbalbalba</h1>
-    </div>
+    <div className="card-content-descirption row-span-1 m-5 flex gap-2 flex-col">
+        <h1 className="font-medium text-2xl">Über uns</h1>
+        <p>Über unsere Geschichte und das Unternehmen</p>
+        <button className="border-[#00704A] border-2 w-1/4 rounded-xl text-[#00704A] px-2 py-1 font-semibold text-sm cursor-pointer hover:bg-[#00704A]/20">Erfahre mehr</button>
+      </div>
     </div>
   )
 };
--- src/components/Blogcards/Card4.tsx
+++ src/components/Blogcards/Card4.tsx
@@ -1,16 +1,18 @@
 import React from "react";
-import picture4 from "../../assets/images/picture4.jpg"
+import picture3 from "../../assets/images/picture3.jpg"
 
 
 const Card4 = () => {
   return (
     <div className="grid grid-cols-1 grid-rows-3">
     <div className="image-ctn row-span-2">
-        <img src={picture4} alt="" />
+        <img src={picture3} alt="" />
     </div>
-    <div className="card-content-descirption row-span-1 m-5">
-        <h1>blablbalbalbalba</h1>
-    </div>
+    <div className="card-content-descirption row-span-1 m-5 flex gap-2 flex-col">
+        <h1 className="font-medium text-2xl">Starbucks Stories</h1>
+        <p>Bleibe auf dem Laufenden mit unseren neusten Stories</p>
+        <button className="border-[#00704A] border-2 w-1/4 rounded-xl text-[#00704A] px-2 py-1 font-semibold text-sm cursor-pointer hover:bg-[#00704A]/20">Erfahre mehr</button>
+      </div>
     </div>
   )
 };
